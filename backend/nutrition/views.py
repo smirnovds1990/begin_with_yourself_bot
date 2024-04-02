12c8676856ac583d304799f4c76ba2752e34df47
Endashi <bariv.work@yandex.ru>
2024-04-02 22:05:34 +0300

    fix pylint

--- backend/nutrition/views.py
+++ backend/nutrition/views.py
@@ -1,11 +1,12 @@
 from datetime import date
 
+from profile.models import UserProfile
+
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from profile.models import UserProfile
 from .constants import ACTIVITY_MODIFIERS, GENDER_MODIFIERS, GOAL_MODIFIERS
 
 

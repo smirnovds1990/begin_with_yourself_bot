c3ab757a5d22b4207ea1ec7ea7917056f6ebd7b2
unknown <nikita.gorelko@mail.ru>
2024-04-03 23:04:59 +0300

    fix to pr

--- backend/sleep/urls.py
+++ backend/sleep/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from sleep.views import SleepView, WakeUpView, get_last_sleep
 
 urlpatterns = [
-    path('<str:username>/sleep/', SleepView.as_view(), name='sleep'),
-    path('<str:username>/wake_up/', WakeUpView.as_view(), name='wake_up'),
-    path('<str:username>/last_sleep/', get_last_sleep, name='wake_up'),
+    path('sleep/', SleepView.as_view(), name='sleep'),
+    path('wake_up/', WakeUpView.as_view(), name='wake_up'),
+    path('last_sleep/', get_last_sleep, name='last_sleep'),
 ]
--- backend/sleep/views.py
+++ backend/sleep/views.py
@@ -1,50 +1,44 @@
 from datetime import datetime
 
-from django.contrib.auth import get_user_model
-from django.shortcuts import get_object_or_404
 from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
-from rest_framework.decorators import api_view
 
 from sleep.models import Sleep
 from sleep.serializers import SleepSerializer
 
-User = get_user_model()
-
 
 class SleepView(APIView):
-    def post(self, request, username):
-        user = get_object_or_404(User, username=username)
-        sleep = Sleep.objects.create(client=user)
+    def post(self, request):
+        sleep = Sleep.objects.create(client=request.user)
         return Response(
-            {'user': user.id, 'sleep_time': sleep.sleep_time},
+            {'user': sleep.client, 'sleep_time': sleep.sleep_time},
             status=status.HTTP_201_CREATED,
         )
 
 
 class WakeUpView(APIView):
-    permission_classes = [AllowAny,]
-
-    def post(self, request, username):
-        user = get_object_or_404(User, username=username)
-        latest_object = Sleep.objects.filter(client=user).first()
-        if latest_object.wake_time:
-            latest_object = Sleep.objects.create(
-                wake_time=datetime.now(), is_sleeping=False, client=user
+    def post(self, request):
+        last_sleep = Sleep.objects.filter(client=request.user).first()
+        if not last_sleep or last_sleep.wake_time:
+            last_sleep = Sleep.objects.create(
+                wake_time=datetime.now(),
+                is_sleeping=False,
+                client=request.user,
             )
         else:
-            latest_object.wake_time = datetime.now()
-            latest_object.is_sleeping = False
-            latest_object.save()
-        serializer = SleepSerializer(latest_object)
+            last_sleep.wake_time = datetime.now()
+            last_sleep.is_sleeping = False
+            last_sleep.save()
+        serializer = SleepSerializer(last_sleep)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET'])
-def get_last_sleep(request, username):
-    user = get_object_or_404(User, username=username)
-    latest_object = Sleep.objects.filter(client=user).first()
-    serializer = SleepSerializer(latest_object)
+def get_last_sleep(request):
+    last_sleep = Sleep.objects.filter(client=request.user).first()
+    if not last_sleep:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = SleepSerializer(last_sleep)
     return Response(serializer.data, status=status.HTTP_200_OK)

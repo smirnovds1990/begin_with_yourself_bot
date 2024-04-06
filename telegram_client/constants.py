8ba6abcc8d01e5c582f1e2da2c3d407004835023
smirnovds1990 <smirnovds1990@gmail.com>
2024-04-06 11:52:12 +0300

    feat: создал отдельную БД в контейнере для бота, подключил к SQLAlchemy, создал модель TelegramUser.

--- telegram_client/constants.py
+++ telegram_client/constants.py
@@ -1,3 +1,10 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 NAMES_PATTERN = r'[А-Яа-я]'
 WIGHT_PATTERN = r'^[0-9]{2,3}?\.?[0-9]{0,2}$'
 HEIGHT_PATTERN = r'^[0-9]{3,3}$'
@@ -8,3 +15,14 @@ MAX_NAME_LENGTH = 25
 MAX_WEIGHT_LENGTH = 3
 MAX_HEIGHT_LENGTH = 5
 DATE_LENGTH = 10
+TOKEN_LENGTH = 100
+
+POSTGRES_USER = getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = getenv('POSTGRES_PASSWORD')
+DB_HOST = getenv('DB_HOST')
+DB_PORT = getenv('DB_PORT')
+POSTGRES_DB = getenv('POSTGRES_DB')
+DATABASE_URL = (
+    f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@'
+    f'{DB_HOST}:{DB_PORT}/{POSTGRES_DB}'
+)
---
+++ telegram_client/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import DeclarativeBase, declared_attr
+
+from .constants import MAX_NAME_LENGTH, TOKEN_LENGTH
+
+
+class Base(DeclarativeBase):
+
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    id = Column(Integer, primary_key=True)
+
+
+class TelegramUser(Base):
+    username = Column(String(MAX_NAME_LENGTH))
+    tg_user_id = Column(Integer)
+    token = Column(String(TOKEN_LENGTH))

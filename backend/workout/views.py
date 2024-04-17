97481b2a0b4aee4c34f87faac10357c9805151ab
Endashi <bariv.work@yandex.ru>
2024-04-18 00:56:23 +0300

    Изменен статус возврата, при отсутствии доступных тренировок

--- backend/workout/views.py
+++ backend/workout/views.py
@@ -311,5 +311,5 @@ class SessionAvailableWorkoutView(BaseUserWorkoutProgramView):
 
         return Response(
             {"message": "Нет доступных тренировок"},
-            status=status.HTTP_200_OK
+            status=status.HTTP_204_NO_CONTENT
         )

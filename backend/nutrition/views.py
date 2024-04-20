fefd8f354be1c4cdda6d49b5e292ab0566fe9c43
Slavashake <rdr32@yandex.ru>
2024-04-20 17:32:54 +0300

    fix for nutrition feature

--- backend/nutrition/views.py
+++ backend/nutrition/views.py
@@ -8,6 +8,12 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .constants import (ACTIVITY_MODIFIERS,
+                        CALORIES_PROTEIN,
+                        CALORIES_SUPPLEMENT,
+                        CALORIES_FAT,
+                        COEFFICIENT_PROTEIN,
+                        COEFFICIENT_SUPPLEMENT,
+                        COEFFICIENT_FAT,
                         GENDER_MODIFIERS,
                         GOAL_MODIFIERS)
 
@@ -33,5 +39,21 @@ class CalorieNormView(APIView):
 
         calories *= activity_modifier
         calories *= goal_modifier
+        calories = round(calories)
 
-        return Response({'calories_norm': calories})
+        protein = round(
+            calories * COEFFICIENT_PROTEIN / CALORIES_PROTEIN
+        )
+        fat = round(
+            calories * COEFFICIENT_FAT / CALORIES_FAT
+        )
+        supplement = round(
+            calories * COEFFICIENT_SUPPLEMENT / CALORIES_SUPPLEMENT
+        )
+
+        return Response({
+            'calories_norm': calories,
+            'protein': protein,
+            'fat': fat,
+            'supplement': supplement
+        })
